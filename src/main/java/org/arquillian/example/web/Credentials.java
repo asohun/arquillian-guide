217907002d70050853fb1dc3ccbf30b6f10cd937
asohun <anoop.sohun@gmail.com>
2015-03-28 14:08:22 +0400

    Minor changes

--- src/main/java/org/arquillian/example/web/Credentials.java
+++ src/main/java/org/arquillian/example/web/Credentials.java
@@ -2,23 +2,51 @@ package org.arquillian.example.web;
 
 import javax.enterprise.inject.Model;
 
+/**
+ * @author asohun
+ * @version 28/03/2015
+ */
 @Model
 public class Credentials {
+
+	/**
+	 * The String representing the login of the user
+	 */
 	private String username;
+
+	/**
+	 * The String representing the user's password
+	 */
 	private String password;
 
+	/**
+	 * @return {@link org.arquillian.example.web.Credentials#username} the
+	 *         String to return
+	 */
 	public String getUsername() {
 		return username;
 	}
 
+	/**
+	 * @param username
+	 *            the string to set
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
+	/**
+	 * @return {@link org.arquillian.example.web.Credentials#password} the
+	 *         String to return
+	 */
 	public String getPassword() {
 		return password;
 	}
 
+	/**
+	 * @param password
+	 *            the String to return
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
--- src/main/java/org/arquillian/example/web/User.java
+++ src/main/java/org/arquillian/example/web/User.java
@@ -1,20 +1,43 @@
 package org.arquillian.example.web;
 
+/**
+ * @author asohun
+ * @version 28/05/2013
+ */
 public class User {
+
+	/**
+	 * The String representing the user's login
+	 */
 	private String username;
 
+	/**
+	 * Default constructor
+	 */
 	public User() {
 
 	}
 
+	/**
+	 * @param username
+	 *            the String to set
+	 */
 	public User(String username) {
 		this.username = username;
 	}
 
+	/**
+	 * @return {@link org.arquillian.example.web.User#username} the String to
+	 *         return
+	 */
 	public String getUsername() {
 		return username;
 	}
 
+	/**
+	 * @param username
+	 *            the String to set
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
